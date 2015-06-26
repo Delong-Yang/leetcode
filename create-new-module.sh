dc8b2975f871f0a7954166bd299913cdd254fac3
Delong-Yang <ydl19@126.com>
2015-06-26 17:45:06 +0800

    escape quotes in content

--- create-new-module.sh
+++ create-new-module.sh
@@ -10,10 +10,10 @@ fi
 mkdir ${module_name}
 cd ${module_name}
 
-echo "<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+echo  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<project xmlns=\"http://maven.apache.org/POM/4.0.0\"
+         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">
     <parent>
         <artifactId>parent</artifactId>
         <groupId>com.delong.leetcode</groupId>
@@ -29,22 +29,22 @@ mkdir -p src/main/java
 mkdir -p src/main/resources
 mkdir -p src/test/groovy
 
-echo "import spock.lang.Specification
+echo \"import spock.lang.Specification
 
 class SolutionTest extends Specification {
-    def "test"() {
+    def \"test\"() {
         setup:
         
         when:
 
         then:
     }
-}" >> src/test/groovy/SolutionTest.groovy
+}\" >> src/test/groovy/SolutionTest.groovy
 
-echo "
+echo \"
 public class Solution {
     
-}" >> src/main/java/Solution.java
+}\" >> src/main/java/Solution.java
 
-echo "${module_name} created!"
+echo \"${module_name} created!\"
 
