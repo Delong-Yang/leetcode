b42935e244c7ee65005f0817334bc5c86cd88a08
Delong <ydl19@126.com>
2017-07-02 14:56:46 +0800

    add module 0236

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/src/test/groovy/SolutionTest.groovy
@@ -0,0 +1,12 @@
+import spock.lang.Specification
+
+class SolutionTest extends Specification {
+    def "test"() {
+        setup:
+        def solution = new Solution()
+
+        when:
+
+        then:
+    }
+}
